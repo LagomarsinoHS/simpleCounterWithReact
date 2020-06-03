500b3fc5809934854d86ee358aa2371032ca959d
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-06-03 17:02:43 -0400

    upload archivos

---
+++ src/components/lights.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+export function Lights(props) {
+    const [active, setActive] = useState("");
+
+    function cambiaClase(e) {
+        console.log("Tengo la clase: ", e.target.className);
+        setActive(e.target.className);
+
+
+    }
+    /* //If normal
+    let gActive= "";
+    if(active=="green-light"){
+        gActive="gActive";
+    }
+    */
+    //if Ternario tiene que ser asignada a algo
+    let gActive = active == "green-light" ? "gActive" : "";
+
+
+    return (
+        <div className="semaforo">
+            <div className={"red-light" + (active === "red-light" ? " rActive" : "")} onClick={(e) => cambiaClase(e)}></div>
+            <div className={"yellow-light" + (active === "yellow-light" ? " yActive" : "")} onClick={(e) => cambiaClase(e)}></div>
+            <div className={`green-light ${gActive}`} onClick={(e) => cambiaClase(e)}></div>
+
+        </div>
+    )
+}
---
+++ src/components/semaforo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Lights } from "./lights"
+
+export function Semaforo() {
+    return <>
+        <h1>Traffic Light</h1>
+        <Lights />
+    </>
+}
